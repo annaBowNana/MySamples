eb6a787eeea3719b16836eb7a4f12bcd67e3e4db
annaBowNana <axcheong@gmail.com>
2017-03-23 18:01:56 -0700

    added codeTask

---
+++ CodeTask/Api controller/UsersApiController.cs
@@ -0,0 +1,46 @@
+﻿using AnnaCodeTask.Domain;
+using AnnaCodeTask.Models;
+using AnnaCodeTask.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AnnaCodeTask.Controllers.Api
+{
+    [RoutePrefix("api/users")]
+    public class UsersApiController : ApiController
+    {
+        [Route("get"), HttpGet]
+        public HttpResponseMessage GetUsers()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            List<UserProfile> response = UserService.GetUserInfo();
+            return Request.CreateResponse(response);
+
+        }
+        [Route("insert"), HttpPost]
+        public HttpResponseMessage InsertUser(InsertUserRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            List<UserProfile> response = new List<UserProfile>();
+            
+             UserService list = new UserService();
+            // response = list.InsertUserProfile(model);
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+
+        }
+
+    }
+}
---
+++ CodeTask/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using AnnaCodeTask.Domain;
+using AnnaCodeTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AnnaCodeTask.Services
+{
+    public class UserService : BaseService
+    {
+        public void InsertUserProfile(InsertUserRequest model)
+        {
+            DataProvider.ExecuteNonQuery(GetConnection, "storedProcName"
+             , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+             {
+                 paramCollection.AddWithValue("@FirstName", model.FirstName);
+                 paramCollection.AddWithValue("@LastName", model.LastName);
+
+             }
+             );
+        }
+
+        public static List<UserProfile> GetUserInfo()
+        {
+            List<UserProfile> list = null;
+
+            DataProvider.ExecuteCmd(GetConnection, "dbo.Employees_Select_v2"
+              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+              {
+
+              }, map: delegate (IDataReader reader, short set)
+              {
+                  UserProfile up = new UserProfile();
+                  int startingIndex = 0;
+                  up.EmployeeId = reader.GetInt32(startingIndex++);
+                  up.FirstName = reader.GetString(startingIndex++);
+                  up.LastName = reader.GetString(startingIndex++);
+
+                  if (list == null)
+                  {
+                      list = new List<UserProfile>();
+                  }
+                  list.Add(up);
+
+              }
+           );
+            return list;
+        }
+
+    }
+}
